86bf33332133cfe1e33c0e1ef00dfb1bb1a256b5
KriptoBaba382 <199w29c@mail.ru>
2025-10-06 15:42:17 +0300

    add: bagfix

--- script.js
+++ script.js
@@ -5,58 +5,58 @@ const todoCompleted = document.querySelector(".todo-completed");
 const todoRemove = document.querySelector(".todo-remove");
 
 let toDoData = JSON.parse(localStorage.getItem("toDoData-local"));
-if (toDoData === null);
-{
-  toDoData = [];
+if (toDoData === null) {
+    toDoData = []
 }
+console.log(toDoData)
 const render = function () {
-  todoList.innerHTML = "";
-  todoCompleted.innerHTML = "";
+    todoList.innerHTML = "";
+    todoCompleted.innerHTML = "";
 
-  toDoData.forEach(function (item) {
-    const li = document.createElement("li");
+    toDoData.forEach(function (item) {
+        const li = document.createElement("li");
 
-    li.classList.add("todo-item");
+        li.classList.add("todo-item");
 
-    li.innerHTML =
-      '<span class="text-todo">' +
-      item.text +
-      "</span>" +
-      '<div class="todo-buttons">' +
-      '<button class="todo-remove"></button>' +
-      '<button class="todo-complete"></button>' +
-      "</div>";
-    if (item.completed) {
-      todoCompleted.append(li);
-    } else {
-      todoList.append(li);
-    }
-    li.querySelector(".todo-complete").addEventListener("click", function () {
-      item.completed = !item.completed;
-      localStorage.setItem("toDoData-local", JSON.stringify(toDoData));
-      render();
+        li.innerHTML =
+            '<span class="text-todo">' +
+            item.text +
+            "</span>" +
+            '<div class="todo-buttons">' +
+            '<button class="todo-remove"></button>' +
+            '<button class="todo-complete"></button>' +
+            "</div>";
+        if (item.completed) {
+            todoCompleted.append(li);
+        } else {
+            todoList.append(li);
+        }
+        li.querySelector(".todo-complete").addEventListener("click", function () {
+            item.completed = !item.completed;
+            localStorage.setItem("toDoData-local", JSON.stringify(toDoData));
+            render();
+        });
+        li.querySelector(".todo-remove").addEventListener("click", function () {
+            const indexDeleted = toDoData.indexOf(item);
+            toDoData.splice(indexDeleted, 1);
+            localStorage.setItem("toDoData-local", JSON.stringify(toDoData));
+            render();
+        });
     });
-    li.querySelector(".todo-remove").addEventListener("click", function () {
-      const indexDeleted = toDoData.indexOf(item);
-      toDoData.splice(indexDeleted, 1);
-      localStorage.setItem("toDoData-local", JSON.stringify(toDoData));
-      render();
-    });
-  });
 };
-
+render()
 todoControl.addEventListener("submit", function (event) {
-  event.preventDefault();
-  if (headerInput.value == "") {
-    retern;
-  }
-  const newToDo = {
-    text: headerInput.value,
-    completed: false,
-  };
-  toDoData.push(newToDo);
-  localStorage.setItem("toDoData-local", JSON.stringify(toDoData));
-  headerInput.value = "";
+    event.preventDefault();
+    if (headerInput.value == "") {
+        return;
+    }
+    const newToDo = {
+        text: headerInput.value,
+        completed: false,
+    };
+    toDoData.push(newToDo);
+    localStorage.setItem("toDoData-local", JSON.stringify(toDoData));
+    headerInput.value = "";
 
-  render();
+    render();
 });
